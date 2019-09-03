6b110c67735982116123de5f95c70af6d5b98841
gavinblack87 <gavin_black@hotmail.co.uk>
2019-09-03 14:50:21 +0100

    mvp done

--- src/main/java/Bus.java
+++ src/main/java/Bus.java
@@ -1,10 +1,35 @@
+import java.util.ArrayList;
+
 public class Bus {
+    private String destination;
+    private int capacity;
+    private ArrayList<Passenger> passengers;
+
+    public Bus(String destination, int capacity) {
+        this.destination = destination;
+        this.capacity = capacity;
+        this.passengers = new ArrayList<Passenger>(capacity);
+    }
+
     public String getDestination() {
+        return this.destination;
     }
 
     public int getCapacity() {
+        return this.capacity;
     }
 
     public int getPassengerCount() {
+        return this.passengers.size();
+    }
+
+    public void addPassenger(Passenger passenger) {
+        if (this.passengers.size() < this.capacity) {
+            this.passengers.add(passenger);
+        }
+    }
+
+    public void removePassenger() {
+        this.passengers.remove(0);
     }
 }
--- src/test/java/BusTest.java
+++ src/test/java/BusTest.java
@@ -7,17 +7,40 @@ public class BusTest {
 
     Bus bus;
     Passenger passenger;
+    Passenger passenger2;
 
     @Before
-    public void before(){
+    public void setUp(){
         bus = new Bus("Edinburgh", 100);
         passenger = new Passenger();
+        passenger2 = new Passenger();
     }
 
     @Test
     public void checkBusSetup(){
         assertEquals("Edinburgh", bus.getDestination());
         assertEquals(100, bus.getCapacity());
+        assertEquals(0, bus.getPassengerCount());
+    }
+
+    @Test
+    public void addPassenger(){
+        bus.addPassenger(passenger);
+        assertEquals(1, bus.getPassengerCount());
+    }
+
+    @Test
+    public void overCapacity() {
+        bus = new Bus("Edinburgh", 1);
+        bus.addPassenger(passenger);
+        bus.addPassenger(passenger2);
+        assertEquals(1, bus.getPassengerCount());
+    }
+
+    @Test
+    public void removePassenger() {
+        bus.addPassenger(passenger);
+        bus.removePassenger();
         assertEquals(0, bus.getPassengerCount());
     }
 }
